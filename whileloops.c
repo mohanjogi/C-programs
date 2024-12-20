4165959ce5a163ae7f116092391a6628234fd0e4
mohanjogi <mohankrishnajogi123@gmail.com>
2024-12-20 11:53:05 +0530

    Create whileloops.c

---
+++ whileloops.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() 
+{
+    int i,j;
+    i=0;
+    j=10;
+    printf("enter i and j values:");
+    scanf("%d%d",&i,&j);
+    i=0;
+    while(i<=10)
+    {
+        i++;
+    }
+    j=10;
+    while(j>=0)
+    {
+        j--;
+    }
+    }
